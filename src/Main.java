54d3d9d3144eeef1788614d35acd3b0c0ff8bf9f
unknown <sangnhanma14@gmail.com>
2022-01-05 14:00:10 +0700

    add Main.java file

--- src/Main.java
+++ src/Main.java
@@ -3,28 +3,31 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         int count = 0;
+        int numbers = 0;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the number:");
         int number = scanner.nextInt();
-        for (int numbers = 2; count < number; numbers++) {
+        while (count<number){
             if (numbers < 2) {
+                numbers++;
                 continue;
             } else if (numbers == 2) {
                 System.out.println(numbers);
                 count++;
             } else {
                 boolean checkPrime = true;
-                for (int i = 2; i < numbers; i++) {
+                for (int i = 2; i < Math.sqrt(numbers); i++) {
                     if (numbers % i == 0) {
                         checkPrime = false;
                         break;
                     }
                 }
-                if (checkPrime == true) {
-                    System.out.println(numbers);
+                if (checkPrime) {
                     count++;
+                    System.out.println(numbers);
                 }
             }
+            numbers++;
         }
     }
 }
